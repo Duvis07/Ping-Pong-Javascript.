fac2ed45862b51c7be571fc677b56795b2a041dc
Duvis07 <96325513+Duvis07@users.noreply.github.com>
2022-04-26 18:55:46 -0500

    Se crean  funciones finales para el juego
    Se crean  funciones al juego correspondientes  para que el juego funcione correctamente, además se le da los estilos con Css

--- Practica_Javascript/main.js
+++ Practica_Javascript/main.js
@@ -2,7 +2,7 @@
  * Esta funcion va tener toda la propiedad del juego
  */
 var game = (function () {
-  let time = 30;
+  let time = 50;
   let movement = 20;
   let movementBar = 20;
   let width = document.documentElement.clientWidth - movement;
@@ -12,7 +12,7 @@ var game = (function () {
   let player2;
   /**
    * el intervalo de juego se ejecuta al momento ejecutar
-   * funcion princioal
+   * funcion principal
    */
   function start() {
     init();
@@ -34,7 +34,103 @@ var game = (function () {
     player2.keyPress = false;
     player2.keyCode = null;
   }
-  
+  /**
+   * Funcion que me evalua cuando la pelota sale del tablero
+   */
+  function stop() {
+    clearInterval(controlGame);
+    document.body.style.background = "#f00";
+  }
+
+  function play() {
+    moveBall();
+    moveBar();
+    checkIfLost();
+  }
+  /**
+   *Funcion que evalua cuando pierde uno  de los palyers
+   */
+
+  function checkIfLost() {
+    if (ball.offsetLeft >= width) {
+      stop();
+      console.log("punto player 1");
+    }
+    if (ball.offsetLeft <= 0) {
+      stop();
+      console.log("punto player 2");
+    }
+  }
+  /**
+   * Funcion para darle el  movimiento a la pelota y la direccion
+   */
+
+  function moveBall() {
+    checkStateBall();
+    switch (ball.state) {
+      case 1: // derecha, abajo
+        ball.style.left = ball.offsetLeft + movement + "px";
+        ball.style.top = ball.offsetTop + movement + "px";
+        break;
+      case 2: // derecha, arriba
+        ball.style.left = ball.offsetLeft + movement + "px";
+        ball.style.top = ball.offsetTop - movement + "px";
+        break;
+      case 3: // izquierda, abajo
+        ball.style.left = ball.offsetLeft - movement + "px";
+        ball.style.top = ball.offsetTop + movement + "px";
+        break;
+      case 4: // izquierda, arriba
+        ball.style.left = ball.offsetLeft - movement + "px";
+        ball.style.top = ball.offsetTop - movement + "px";
+        break;
+    }
+  }
+  /**
+   * Funcion  que evalua cuando la pelota choca contra las barras
+   */
+
+  function checkStateBall() {
+    if (collidePlayer2()) {
+      ball.direction = 2;
+      if (ball.state == 1) ball.state = 3;
+      if (ball.state == 2) ball.state = 4;
+    } else if (collidePlayer1()) {
+      ball.direction = 1;
+      if (ball.state == 3) ball.state = 1;
+      if (ball.state == 4) ball.state = 2;
+    }
+
+    if (ball.direction === 1) {
+      if (ball.offsetTop >= height) ball.state = 2;
+      else if (ball.offsetTop <= 0) ball.state = 1;
+    } else {
+      if (ball.offsetTop >= height) ball.state = 4;
+      else if (ball.offsetTop <= 0) ball.state = 3;
+    }
+  }
+
+  function collidePlayer1() {
+    if (
+      ball.offsetLeft <= bar1.clientWidth &&
+      ball.offsetTop >= bar1.offsetTop &&
+      ball.offsetTop <= bar1.offsetTop + bar1.clientHeight
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+  function collidePlayer2() {
+    if (
+      ball.offsetLeft >= width - bar2.clientWidth &&
+      ball.offsetTop >= bar2.offsetTop &&
+      ball.offsetTop <= bar2.offsetTop + bar2.clientHeight
+    ) {
+      return true;
+    }
+    return false;
+  }
 
   /**
    * Funcion para mover las barras por medio de las teclas
