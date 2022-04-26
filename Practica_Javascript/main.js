ef4cdfddf5790c00e18727ed5bf44fb939cdbbd7
Duvis07 <96325513+Duvis07@users.noreply.github.com>
2022-04-26 13:54:20 -0500

    Funcion para detectar las teclas presionadas
    Se crea Función para mover las barras por medio de las teclas y además captura las teclas que presiona el player1 y player2

--- Practica_Javascript/main.js
+++ Practica_Javascript/main.js
@@ -18,11 +18,11 @@ var game = (function () {
     init();
     controlGame = setInterval(play, time);
   }
-/**
- * funcion que nos define la direccion  de la pelota
- * y los estados iniciales del juego
- * se crean los objetos player1 y player2
- */
+  /**
+   * funcion que nos define la direccion  de la pelota
+   * y los estados iniciales del juego
+   * se crean los objetos player1 y player2
+   */
   function init() {
     ball.style.left = 0;
     ball.state = 2;
@@ -34,4 +34,57 @@ var game = (function () {
     player2.keyPress = false;
     player2.keyCode = null;
   }
+  /**
+   *
+   */
 
+  /**
+   * Funcion para mover las barras por medio de las teclas
+   * captura las teclas que presiona el player
+   */
+  function moveBar() {
+    if (player1.keyPress) {
+      if (player1.keyCode == 81 && bar1.offsetTop >= 0)
+        bar1.style.top = bar1.offsetTop - movementBar + "px";
+      if (player1.keyCode == 65 && bar1.offsetTop + bar1.clientHeight <= height)
+        bar1.style.top = bar1.offsetTop + movementBar + "px";
+    }
+    if (player2.keyPress) {
+      if (player2.keyCode == 79 && bar2.offsetTop >= 0)
+        bar2.style.top = bar2.offsetTop - movementBar + "px";
+      if (player2.keyCode == 76 && bar2.offsetTop + bar2.clientHeight <= height)
+        bar2.style.top = bar2.offsetTop + movementBar + "px";
+    }
+  }
+  /**
+   * funcion cuando usuario presiona la tecla
+   * @param {*} e
+   */
+  document.onkeydown = function (e) {
+    e = e || window.event;
+    switch (e.keyCode) {
+      case 81: // Q
+      case 65: // A
+        player1.keyCode = e.keyCode;
+        player1.keyPress = true;
+        break;
+      case 79: // O
+      case 76: // L
+        player2.keyCode = e.keyCode;
+        player2.keyPress = true;
+        break;
+    }
+  };
+  /**
+   * Funcion para detectar cuando el  usuario despresiona
+   * las teclas
+   *
+   * @param {*} e
+   */
+  document.onkeyup = function (e) {
+    if (e.keyCode == 81 || e.keyCode == 65) player1.keyPress = false;
+    if (e.keyCode == 79 || e.keyCode == 76) player2.keyPress = false;
+  };
+
+  start();
+})();
